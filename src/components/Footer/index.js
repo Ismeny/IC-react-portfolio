6a079e246aa42a6db4920bfb7dd007ae380ecd1f
Ismeny Saguilan <menysag2@gmail.com>
2022-02-01 21:41:36 -0600

    react app portfolio template

---
+++ src/components/Footer/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Footer() {
+
+  // Replace links with social media profiles
+  const icons = [
+    {
+      name: "fab fa-github",
+      link: "https://github.com/Ismeny"
+    },
+    {
+      name: "fab fa-linkedin",
+      link: "https://www.linkedin.com/in/ismeny-castro-b8b32821b/"
+    },
+    {
+      name: "fab fa-twitter",
+      link: "https://twitter.com/ismenycastro"
+    }
+  ]
+
+  return (
+    <footer className="flex-row px-1">
+      {icons.map(icon =>
+      (
+        <a href={icon.link} key={icon.name} target="_blank" rel="noopener noreferrer"><i className={icon.name}></i></a>
+      )
+        )}
+    </footer>
+  );
+}
+
+export default Footer;
