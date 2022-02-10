b763bbdd964095a4249e13601b51d5452890bf69
Ismeny Saguilan <menysag2@gmail.com>
2022-02-10 12:47:49 -0600

    updated linkedin link

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,7 +10,7 @@ function Footer() {
     },
     {
       name: "fab fa-linkedin",
-      link: "https://www.linkedin.com/in/ismeny-castro-b8b32821b/"
+      link: "https://www.linkedin.com/in/ismenycastro/"
     },
     {
       name: "fab fa-twitter",
